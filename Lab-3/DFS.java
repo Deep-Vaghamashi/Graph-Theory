6f3fb090cca1318b415a44768ca034dc7a88f69e
Deep-Vaghamashi <vaghamashideep@gmail.com>
2025-06-23 19:33:56 +0530

    DFS
    DFS with and without recursive

--- Lab-3/DFS.java
+++ Lab-3/DFS.java
@@ -3,6 +3,11 @@ import java.util.*;
 
 public class DFS {
 
+    static int[][] graph = new int[][]{
+        {1, 2}, {0, 2}, {0, 1, 3, 4}, {2}, {2}
+    };
+    static boolean[] visited = new boolean[5];
+
     public static void main(String[] args) {
         int[][] graph = new int[][]{
             {1, 2}, {0, 2}, {0, 1, 3, 4}, {2}, {2}
@@ -24,6 +29,19 @@ public class DFS {
                     stack.push(neighbors[n]);
                     visited[neighbors[n]] = true;
                 }
+            }
+        }
+
+        System.out.println();
+        DFSReg(0);
+    }
+
+    public static void DFSReg(int n) {
+        visited[n] = true;
+        System.out.print(n + " ");
+        for (int i : graph[n]) {
+            if (!visited[i]) {
+                DFSReg(i);
             }
         }
     }
