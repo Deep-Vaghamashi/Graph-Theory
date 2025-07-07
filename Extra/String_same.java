1f782b3d9322543bd6e20371e397a56b3f52febd
Deep-Vaghamashi <vaghamashideep@gmail.com>
2025-07-07 09:28:21 +0530

    .

---
+++ Extra/String_same.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class String_same {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a string in the form of ['(',')'] :");
+        String a = sc.next();
+    }
+    // public static boolean S1 (String a){
+    //     if(a.length()%2==1){
+
+    //     }
+    //     else{
+    //     }
+    // }
+}
